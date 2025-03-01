d889bf0a199f1f2e379333579df28138b0b93dd1
felipeotarola <feot1000@gmail.com>
2025-03-01 21:12:52 +0100

    ignoreDuringBuilds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint errors during production builds
+  },
 };
 
 export default nextConfig;
